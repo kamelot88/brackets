dd02cef877c3947903ef216cee5b67bca98835c1
kamelot88 <vasilchenkoOV1988@gmail.com>
2021-03-15 23:59:48 +0200

    finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+      const newAr = [],
+            array = str.split("");
+      for (let i = 0; i < array.length; i++) {
+          for (let j = 0; j < bracketsConfig.length; j++) {
+  
+              if (array[i] === bracketsConfig[j][0]) {
+                  if (bracketsConfig[j][0] === bracketsConfig[j][1]
+                      && newAr[newAr.length - 1] === bracketsConfig[j][0]) {
+                        newAr.pop();
+                  } else {
+                    newAr.push(array[i]);
+                  }
+              } else if (array[i] === bracketsConfig[j][1]) {
+  
+                  if (newAr[newAr.length - 1] !== bracketsConfig[j][0]) {
+                      return false;
+                  } else {
+                    newAr.pop();
+                  }
+              } else {
+              }
+          }
+      }
+      return (newAr.length === 0);
